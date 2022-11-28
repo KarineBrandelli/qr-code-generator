0de7514ded41dbe2a4f9c36a0a851f886a86d923
Karine Brandelli <karinebrandelli@live.com>
2022-11-28 15:47:29 -0300

    feat: add alert for when input is empty

--- js/script.js
+++ js/script.js
@@ -2,36 +2,39 @@ const input = document.querySelector('#url-text');
 const button = document.querySelector('#generate');
 
 button.addEventListener('click', (event) => {
-  console.log(input.value)
   generateQRCode(input);
-})
+});
 
 function generateQRCode(input) {
-  const qrCode = new QRCodeStyling({
-    width: 290,
-    height: 290,
-    type: "svg",
-    data: `"${input.value}"`,
-    image: "./assets/github.svg",
-    dotsOptions: {
-      color: "#3685FE",
-      type: "classy"
-    },
-    backgroundOptions: {
-      color: "#e9ebee",
-    },
-    imageOptions: {
-      crossOrigin: "anonymous",
-      margin: 5,
-      imageSize: 0.3
-    },
-    qrOptions: {
-      errorCorrectionLevel: "H"
-    },
-    cornersSquareOptions: {
-      color: "#225cb4"
-    }
-  });
-  
-  qrCode.append(document.querySelector(".container-image"));
+  if (input.value != '') {
+    const qrCode = new QRCodeStyling({
+      width: 290,
+      height: 290,
+      type: "svg",
+      data: `"${input.value}"`,
+      image: "./assets/github.svg",
+      dotsOptions: {
+        color: "#3685FE",
+        type: "classy"
+      },
+      backgroundOptions: {
+        color: "#e9ebee",
+      },
+      imageOptions: {
+        crossOrigin: "anonymous",
+        margin: 5,
+        imageSize: 0.3
+      },
+      qrOptions: {
+        errorCorrectionLevel: "H"
+      },
+      cornersSquareOptions: {
+        color: "#225cb4"
+      }
+    });
+    
+    qrCode.append(document.querySelector(".container-image"));
+  } else {
+    alert('Input cannot be empty.')
+  }
 }
