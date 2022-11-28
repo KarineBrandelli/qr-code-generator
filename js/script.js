061972d7a18fca3afdcda25146c8edc530981b4b
Karine Brandelli <karinebrandelli@live.com>
2022-11-28 14:50:49 -0300

    add code to generate qrcode

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const input = document.querySelector('#url-text');
+const button = document.querySelector('#generate');
+
+button.addEventListener('click', (event) => {
+  console.log(input.value)
+  generateQRCode(input.value);
+})
+
+function generateQRCode(input) {
+  const qrCode = new QRCodeStyling({
+    width: 300,
+    height: 300,
+    type: "svg",
+    data: `${input.value}`,
+    image: "./assets/github.svg",
+    dotsOptions: {
+        color: "#4267b2",
+        type: "rounded"
+    },
+    backgroundOptions: {
+        color: "#e9ebee",
+    },
+    imageOptions: {
+        crossOrigin: "anonymous",
+        margin: 5,
+        imageSize: 0.3
+    }
+  });
+  
+  qrCode.append(document.querySelector(".container-image"));
+}
+
+
+
+
