408748314c32600e3de4d1e1fd5e619cd267d2ea
Karine Brandelli <karinebrandelli@live.com>
2022-12-02 14:43:46 -0300

    build: start function to upload image

--- js/script.js
+++ js/script.js
@@ -4,20 +4,27 @@ const inputImageName = document.querySelector('.image-name');
 const button = document.querySelector('#generate');
 const modal = document.querySelector('.modal');
 const download = document.querySelector('.download');
-console.log(inputImage)
 
 button.addEventListener('click', (event) => {
-  generateQRCode(input);
+  generateQRCode(input, inputImage);
 });
 
-function generateQRCode(input) {
+inputImage.addEventListener('change', (event) => {
+  inputImageName.value = inputImage.value;
+
+  console.log(inputImageName.value)
+  console.log(inputImage.value)
+  console.log(inputImage)
+});
+
+function generateQRCode(input, inputImage) {
   if (input.value != '') {
     const qrCode = new QRCodeStyling({
       width: 250,
       height: 250,
       type: "jpeg",
-      data: `"${input.value}"`,
-      image: "",
+      data: `${input.value}`,
+      image: `${inputImage.value}`,
       dotsOptions: {
         color: "#3685FE",
         type: "classy"
